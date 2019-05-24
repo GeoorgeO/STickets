29f5d0d8b1d2234e58bc4dc0f4087050f51981b0
Juan Alvaro Esquivel Aguilar <iscjaea@gmail.com>
2019-05-24 13:36:26 -0500

    Cambios

---
+++ Scripts/Procedimientos Almacenados/STic_CatPantallas_Select.sql
@@ -0,0 +1,36 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+IF  EXISTS (SELECT * FROM SYS.OBJECTS WHERE TYPE = 'P' AND NAME = 'STic_CatPantallas_Select')
+DROP PROCEDURE STic_CatPantallas_Select 
+go
+CREATE PROCEDURE STic_CatPantallas_Select
+	
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT        c_codigo_pan, v_nombre_pan
+	FROM            cat_Pantallas
+END
+GO
---
+++ Software/SystemTickets/CapaDeDatos/Clases/CLS_CatPantallas.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDeDatos.Clases
+{
+    public class CLS_CatPantallas:ConexionBase
+    {
+        public string c_codigo_pan { get; set; }
+        public string v_nombre_pan { get; set; }
+
+        public void MtdSeleccionarPantallas()
+        {
+            TipoDato _dato = new TipoDato();
+            Conexion _conexion = new Conexion(cadenaConexionR);
+
+            Exito = true;
+            try
+            {
+                _conexion.EjecutarDataset();
+
+                if (_conexion.Exito)
+                {
+                    Datos = _conexion.Datos;
+                }
+                else
+                {
+                    Mensaje = _conexion.Mensaje;
+                    Exito = false;
+                }
+            }
+            catch (Exception e)
+            {
+                Mensaje = e.Message;
+                Exito = false;
+            }
+
+        }
+        public void MtdInsertarPantalla()
+        {
+            TipoDato _dato = new TipoDato();
+            Conexion _conexion = new Conexion(cadenaConexionR);
+
+            Exito = true;
+            try
+            {
+                _dato.CadenaTexto = v_nombre_pan;
+                _conexion.agregarParametro(EnumTipoDato.CadenaTexto, _dato, "v_nombre_pan");
+                _conexion.EjecutarDataset();
+
+                if (_conexion.Exito)
+                {
+                    Datos = _conexion.Datos;
+                }
+                else
+                {
+                    Mensaje = _conexion.Mensaje;
+                    Exito = false;
+                }
+            }
+            catch (Exception e)
+            {
+                Mensaje = e.Message;
+                Exito = false;
+            }
+
+        }
+        public void MtdActualizarPantalla()
+        {
+            TipoDato _dato = new TipoDato();
+            Conexion _conexion = new Conexion(cadenaConexionR);
+
+            Exito = true;
+            try
+            {
+                _dato.CadenaTexto = c_codigo_pan;
+                _conexion.agregarParametro(EnumTipoDato.CadenaTexto, _dato, "c_codigo_pan");
+                _dato.CadenaTexto = v_nombre_pan;
+                _conexion.agregarParametro(EnumTipoDato.CadenaTexto, _dato, "v_nombre_pan");
+                _conexion.EjecutarDataset();
+
+                if (_conexion.Exito)
+                {
+                    Datos = _conexion.Datos;
+                }
+                else
+                {
+                    Mensaje = _conexion.Mensaje;
+                    Exito = false;
+                }
+            }
+            catch (Exception e)
+            {
+                Mensaje = e.Message;
+                Exito = false;
+            }
+
+        }
+        public void MtdEliminarPantalla()
+        {
+            TipoDato _dato = new TipoDato();
+            Conexion _conexion = new Conexion(cadenaConexionR);
+
+            Exito = true;
+            try
+            {
+                _dato.CadenaTexto = c_codigo_pan;
+                _conexion.agregarParametro(EnumTipoDato.CadenaTexto, _dato, "c_codigo_pan");
+                _conexion.EjecutarDataset();
+
+                if (_conexion.Exito)
+                {
+                    Datos = _conexion.Datos;
+                }
+                else
+                {
+                    Mensaje = _conexion.Mensaje;
+                    Exito = false;
+                }
+            }
+            catch (Exception e)
+            {
+                Mensaje = e.Message;
+                Exito = false;
+            }
+
+        }
+    }
+}
