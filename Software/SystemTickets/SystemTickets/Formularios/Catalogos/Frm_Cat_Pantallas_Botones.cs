abcf0907caad7830e084d734fa36a48c1836c93d
Lalo091 <lalobrock@gmail.com>
2019-05-24 14:35:55 -0500

    pantalla de pantalla boton
    pantalla boton

---
+++ Software/SystemTickets/SystemTickets/Formularios/Catalogos/Frm_Cat_Pantallas_Botones.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace SystemTickets.Formularios.Catalogos
+{
+    public partial class Frm_Cat_Pantallas_Botones : DevExpress.XtraEditors.XtraForm
+    {
+        public Frm_Cat_Pantallas_Botones()
+        {
+            InitializeComponent();
+        }
+
+        private void labelControl2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
