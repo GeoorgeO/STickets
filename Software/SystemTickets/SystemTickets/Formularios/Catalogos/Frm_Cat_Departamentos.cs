fe099f19d6d8587c014d75ee62d5d821ecc3cb96
Lalo091 <lalobrock@gmail.com>
2019-05-24 13:57:50 -0500

    pantalla departamentos
    departamentos

---
+++ Software/SystemTickets/SystemTickets/Formularios/Catalogos/Frm_Cat_Departamentos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace SystemTickets.Formularios.Catalogos
+{
+    public partial class Frm_Cat_Departamentos : DevExpress.XtraEditors.XtraForm
+    {
+        public Frm_Cat_Departamentos()
+        {
+            InitializeComponent();
+        }
+
+        private void labelControl3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
